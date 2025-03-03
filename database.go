599b7ad53d9fa221999b178cdc486c658a1e7577
fyasmine <yasmine.fakir@ynov.com>
2025-03-03 10:25:10 +0100

    auth.go

---
+++ database.go
@@ -0,0 +1,92 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+// Initialisation de la base de données
+func InitDB() {
+	var err error
+	DB, err = sql.Open("sqlite3", "forum.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	createTables()
+}
+
+// Création des tables
+func createTables() {
+	usersTable := `CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		username TEXT UNIQUE NOT NULL,
+		email TEXT UNIQUE NOT NULL,
+		password TEXT NOT NULL,
+		role TEXT DEFAULT 'user'
+	);`
+
+	postsTable := `CREATE TABLE IF NOT EXISTS posts (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		user_id INTEGER,
+		title TEXT NOT NULL,
+		content TEXT NOT NULL,
+		image TEXT,
+		category TEXT,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY(user_id) REFERENCES users(id)
+	);`
+
+	commentsTable := `CREATE TABLE IF NOT EXISTS comments (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		user_id INTEGER,
+		post_id INTEGER,
+		content TEXT NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY(user_id) REFERENCES users(id),
+		FOREIGN KEY(post_id) REFERENCES posts(id)
+	);`
+
+	likesTable := `CREATE TABLE IF NOT EXISTS likes (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		user_id INTEGER,
+		post_id INTEGER,
+		is_like BOOLEAN,
+		FOREIGN KEY(user_id) REFERENCES users(id),
+		FOREIGN KEY(post_id) REFERENCES posts(id)
+	);`
+
+	notificationsTable := `CREATE TABLE IF NOT EXISTS notifications (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		user_id INTEGER,
+		message TEXT NOT NULL,
+		is_read BOOLEAN DEFAULT 0,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY(user_id) REFERENCES users(id)
+	);`
+
+	moderationTable := `CREATE TABLE IF NOT EXISTS moderation (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		moderator_id INTEGER,
+		post_id INTEGER,
+		action TEXT NOT NULL,
+		reason TEXT NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY(moderator_id) REFERENCES users(id),
+		FOREIGN KEY(post_id) REFERENCES posts(id)
+	);`
+
+	tables := []string{usersTable, postsTable, commentsTable, likesTable, notificationsTable, moderationTable}
+
+	for _, table := range tables {
+		_, err := DB.Exec(table)
+		if err != nil {
+			log.Fatal("Erreur lors de la création des tables :", err)
+		}
+	}
+	log.Println("Base de données et tables créées avec succès !")
+}
